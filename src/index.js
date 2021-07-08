97dab2e807eaa00157aebbf0f61e057aa4f65c8e
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-07-08 18:05:25 +0700

    add report full

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./assets/css/animate.min.css";
--- src/views/ReviewManage.js
+++ src/views/ReviewManage.js
@@ -9,8 +9,10 @@ import LayoutReview from "../components/LayoutReview.js";
 import Loading from "../components/Loading.js";
 
 function ReviewManage() {
+  const refSearch = useRef();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(0);
   useEffect(() => {
     Promise.all([axios.get(`${apiLocal}/api/reviews`)])
       .then(([listReview]) => {
@@ -18,7 +20,32 @@ function ReviewManage() {
         setLoading(false);
       })
       .catch();
-  }, []);
+  }, [success]);
+  const searchSchool = (e) => {
+    e.preventDefault();
+    // Promise.all([
+    //   axios.get(`${apiLocal}/api/schools/filter?q=${refSearch.current.value}`),
+    // ])
+    //   .then(([school]) => {
+    //     const x = school.data.schools;
+    //     let arr = [];
+    //     if (school.data.schools !== []) {
+    //       data.filter((item) => {
+    //         if (item.idSchool.code === x[0].code) return arr.push(item);
+    //       });
+    //       setData(arr);
+    //     } else setData([]);
+    //   })
+    //   .catch();
+  };
+  const listReport = () => {
+    let arr = [];
+    data.filter((item) => {
+      if (item.report.count > 0) return arr.push(item);
+    });
+    console.log(arr);
+    setData(arr);
+  };
   return loading ? (
     <div
       className="d-flex align-items-center justify-content-center"
@@ -31,9 +58,30 @@ function ReviewManage() {
       <h2 style={{ borderBottom: "3px solid #0062e0", color: "#014daf" }}>
         Danh sách các bài đánh giá
       </h2>
+      <div style={{ display: "flex" }}>
+        <form onSubmit={(e) => searchSchool(e)}>
+          <input
+            ref={refSearch}
+            placeholder="Tìm kiếm trường theo mã, tên"
+            style={{ width: "360px", height: "100%" }}
+          ></input>
+        </form>
+        <button
+          onClick={() => listReport()}
+          className="btn btn-warning"
+          style={{ marginLeft: "40px" }}
+        >
+          Các bài đánh giá bị báo cáo
+        </button>
+      </div>
       <div>
         {[...data].reverse().map((item, index) => (
-          <LayoutReview item={item} index={index}></LayoutReview>
+          <LayoutReview
+            setSuccess={setSuccess}
+            success={success}
+            item={item}
+            index={index}
+          ></LayoutReview>
         ))}
       </div>
     </>
